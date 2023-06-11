9d538898d0746983db22dce8ec836d888d2d5b80
YosKad <yoskadtl@gmail.com>
2023-06-11 10:49:21 +0300

    give some touch up

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 console.log("hello world");
+function validateInput() {
+    var palindrome = document.getElementById("palindrome");
+    var checkButton = document.getElementById("checkButton");
+    if (palindrome.validity.valid) {
+        checkButton.disabled = false;
+    }
+    else {
+        checkButton.disabled = true;
+    }
+}
 function checkPalindrom() {
     var word = document.getElementById("palindrome").value.toLowerCase();
-    ;
-    var res = "";
+    var result = "";
     for (i = word.length - 1; i >= 0; i--) {
-        res += word[i];
+        result += word[i];
     }
-    if (word == res) {
+    if (word == result) {
         document.getElementById("result").innerHTML = "<strong>Yes</strong>, '".concat(word, "' is a palindrome!");
     }
     else {
--- app.ts
+++ app.ts
@@ -1,26 +1,35 @@
 console.log("hello world");
 
+function validateInput() {
+  var palindrome = document.getElementById("palindrome");
+  var checkButton = document.getElementById("checkButton");
 
-
-function checkPalindrom() {
-    var word = (document.getElementById("palindrome") as HTMLInputElement).value.toLowerCase();;
-    var res="";
-    for(i = word.length-1; i >= 0; i--)
-    {
-        res += word[i];
-    }
-
-    if(word==res)
-    {
-        document.getElementById("result")!.innerHTML = `<strong>Yes</strong>, '${word}' is a palindrome!`;
-    } else {
-        document.getElementById("result")!.innerHTML = `<strong>No</strong>, '${word}' is not a palindrome.`;
-    }
-
-    
+  if (palindrome.validity.valid) {
+    checkButton.disabled = false;
+  } else {
+    checkButton.disabled = true;
+  }
 }
 
+function checkPalindrom() {
+  var word = (
+    document.getElementById("palindrome") as HTMLInputElement
+  ).value.toLowerCase();
+  var result = "";
+  for (i = word.length - 1; i >= 0; i--) {
+    result += word[i];
+  }
 
+  if (word == result) {
+    document.getElementById(
+      "result"
+    )!.innerHTML = `<strong>Yes</strong>, '${word}' is a palindrome!`;
+  } else {
+    document.getElementById(
+      "result"
+    )!.innerHTML = `<strong>No</strong>, '${word}' is not a palindrome.`;
+  }
+}
 
 // console.log(checkPalindrom('hello'));
 // console.log(checkPalindrom('abba'));
