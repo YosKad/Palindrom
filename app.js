7b0f8cd1e98b3c40cf436f12774469c8a776a724
YosKad <yoskadtl@gmail.com>
2023-06-11 10:31:55 +0300

    add html css and make the function work

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 console.log("hello world");
-function checkPalindrom(text) {
-    return text == text.split('').reverse().join('');
+function checkPalindrom() {
+    var word = document.getElementById("palindrome").value.toLowerCase();
+    ;
+    var res = "";
+    for (i = word.length - 1; i >= 0; i--) {
+        res += word[i];
+    }
+    if (word == res) {
+        document.getElementById("result").innerHTML = "<strong>Yes</strong>, '".concat(word, "' is a palindrome!");
+    }
+    else {
+        document.getElementById("result").innerHTML = "<strong>No</strong>, '".concat(word, "' is not a palindrome.");
+    }
 }
-console.log(checkPalindrom('hello'));
-console.log(checkPalindrom('abba'));
+// console.log(checkPalindrom('hello'));
+// console.log(checkPalindrom('abba'));
--- app.ts
+++ app.ts
@@ -1,9 +1,26 @@
 console.log("hello world");
 
 
-function checkPalindrom(text: any): boolean {
-    return text == text.split('').reverse().join('');
+
+function checkPalindrom() {
+    var word = (document.getElementById("palindrome") as HTMLInputElement).value.toLowerCase();;
+    var res="";
+    for(i = word.length-1; i >= 0; i--)
+    {
+        res += word[i];
+    }
+
+    if(word==res)
+    {
+        document.getElementById("result")!.innerHTML = `<strong>Yes</strong>, '${word}' is a palindrome!`;
+    } else {
+        document.getElementById("result")!.innerHTML = `<strong>No</strong>, '${word}' is not a palindrome.`;
+    }
+
+    
 }
 
-console.log(checkPalindrom('hello'));
-console.log(checkPalindrom('abba'));
+
+
+// console.log(checkPalindrom('hello'));
+// console.log(checkPalindrom('abba'));
